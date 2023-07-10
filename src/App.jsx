503126e523c0bb1976c3d939684fe98cfff20121
tranthekhiem14112001 <trankhiem99999@gmail.com>
2023-07-10 08:32:03 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Selector from "./components/Selector.jsx";
+
+function App() {
+    return <Selector/>
+}
+
+export default App
